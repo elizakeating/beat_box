a9eda49b3b4a09b2b6fa8817f44d76f69322d9a4
Eliza Keating <ek.elizakeating@gmail.com>
2023-07-06 21:59:35 -0600

    Change count to method instead of attribute

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,15 +1,26 @@
 class LinkedList
-    attr_reader :head,
-                :count
+    attr_reader :head
 
     def initialize
         @head = nil
-        @count = 0
     end
 
     def append(data)
         @head = Node.new(data)
-        @count += 1
+    end
+
+    def count
+        if @head.nil?
+            count = 0
+        else
+            pointer = @head
+            count = 1
+            while pointer.next_node != nil
+                pointer = pointer.next_node
+                count += 1
+            end
+        end
+        count
     end
 
     def to_string

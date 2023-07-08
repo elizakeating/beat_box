e3cfdca69dc39d3ab68465410f2ff89bfa5e3a80
Eliza Keating <ek.elizakeating@gmail.com>
2023-07-07 20:25:56 -0600

    Remove pry testing lines in beat box test file

--- spec/beat_box_spec.rb
+++ spec/beat_box_spec.rb
@@ -47,10 +47,8 @@ RSpec.describe BeatBox do
             
             expect(bb.count).to eq(6)
             expect(bb.list.count).to eq(6)
-            require 'pry';binding.pry
             
             bb.play
-            require 'pry';binding.pry
         end
     end
 end
